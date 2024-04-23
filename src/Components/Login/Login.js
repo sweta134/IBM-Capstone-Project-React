559c237df454882f563360d66036a3418efa6108
Sweta Upadhyay <upadhyaysweta17@gmail.com>
2024-04-23 11:44:27 +0530

    Week 3 login logic

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,27 +1,91 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import '../Login/Login.css';
+//Apply css according to your design theme or css that has been given to you in week 2 lab 2
 
-export default function Login() {
+import { Link, useNavigate } from 'react-router-dom';
+import { API_URL } from '../../config';
+
+const Login = () => {
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (sessionStorage.getItem('auth-token')) {
+      navigate('/');
+    }
+  }, []);
+
+  const login = async (e) => {
+    e.preventDefault();
+    const res = await fetch(`${API_URL}/api/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        // name: name,
+        email: email,
+        password: password,
+      }),
+    });
+
+    const json = await res.json();
+    if (json.authtoken) {
+      sessionStorage.setItem('auth-token', json.authtoken);
+
+      sessionStorage.setItem('email', email);
+
+      navigate('/');
+      window.location.reload();
+    } else {
+      if (json.errors) {
+        for (const error of json.errors) {
+          alert(error.msg);
+        }
+      } else {
+        alert(json.error);
+      }
+    }
+  };
+
   return (
     <div>
-        <div className="container">
+      <div className="container">
         <div className="login-grid">
           <div className="login-text">
             <h2>Login</h2>
           </div>
           <div className="login-text">
-            Are you a new member? <span><a href="../Sign_Up/Sign_Up.html" style={{color: '#2190FF'}}> Sign Up Here</a></span>
+            Are you a new member?{' '}
+            <span>
+              <Link to="/signup" style={{ color: '#2190FF' }}>
+                {' '}
+                Sign Up Here
+              </Link>
+            </span>
           </div>
           <br />
           <div className="login-form">
-            <form>
+            <form onSubmit={login}>
               <div className="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" name="email" id="email" className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
-                    </div>
-              <div className="form-group">
-                <label for="password" >Password</label>
+                <label htmlFor="email">Email</label>
                 <input
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="form-control"
+                  placeholder="Enter your email"
+                  aria-describedby="helpId"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   type="password"
                   name="password"
                   id="password"
@@ -30,10 +94,13 @@ export default function Login() {
                   aria-describedby="helpId"
                 />
               </div>
-
-
               <div className="btn-group">
-                <button type="submit" className="btn btn-primary mb-2 mr-1 waves-effect waves-light">Login</button> 
+                <button
+                  type="submit"
+                  className="btn btn-primary mb-2 mr-1 waves-effect waves-light"
+                >
+                  Login
+                </button>
                 <button type="reset" className="btn btn-danger mb-2 waves-effect waves-light">Reset</button>
               </div>
               <br />
@@ -45,5 +112,7 @@ export default function Login() {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
+
+export default Login;
