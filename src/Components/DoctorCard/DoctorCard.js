a29660159af7755fdd2c469b95eac3d1fb3f513f
sweta134 <upadhyaysweta17@gmail.com>
2024-04-23 02:59:58 -0400

    Notification week 4 ss

--- src/Components/DoctorCard/DoctorCard.js
+++ src/Components/DoctorCard/DoctorCard.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import './DoctorCard.css';
-import AppointmentFormIC from '../AppointmentFormIC/AppointmentFormIC'
 import { v4 as uuidv4 } from 'uuid';
+import AppointmentFormIC from '../InstantConsultationBooking/InstantConsultationBooking/AppointmentFormIC/AppointmentFormIC';
 
 
 const DoctorCard = ({ name, speciality, experience, ratings, profilePic }) => {
