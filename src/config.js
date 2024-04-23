f2285464b44a333041b5855e220bcc3e4a0ea7ac
sweta134 <upadhyaysweta17@gmail.com>
2024-04-23 05:15:11 -0400

    build

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
-export const API_URL = window.location.hostname === "localhost" ? "https://upadhyayswet-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "https://upadhyayswet-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";
-console.log(
-    "API_URL :",
-    API_URL
-);
+// export const API_URL = window.location.hostname === "localhost" ? "https://upadhyayswet-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "https://upadhyayswet-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";
+// console.log(
+//     "API_URL :",
+//     API_URL
+// );
+
+export const API_URL = window.location.origin;
+console.log("API_URL :", API_URL);
