152158b1634d52cdda2421eb39c295e06f458edf
sweta134 <upadhyaysweta17@gmail.com>
2024-04-23 02:32:05 -0400

    Week 3 consultation

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/LandngPage/Landing_Page';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
+import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultationBooking/InstantConsultation';
+ 
 function App() {
   return (
     <div className="App">
@@ -13,8 +15,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Landing_Page />} />
-          <Route path="/sign_up" element={<Sign_Up />} />
+          <Route path="/signup" element={<Sign_Up />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/instant-consultation" element={<InstantConsultation />} />
         </Routes>
       </BrowserRouter>
     </div>
